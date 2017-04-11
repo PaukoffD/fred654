e135259a6b916df2ac78592a37671ba1ece364e3
xuliaP <paukov2967@gmail.com>
2017-04-11 21:58:54 +0300

    ckeditor production

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,13 @@ module Fred654
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.available_locales = %w(en)
+
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+
+    config.i18n.default_locale = :ru
+    config.autoload_paths += Dir[ Rails.root.join('app', 'models', "commands") ]
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
