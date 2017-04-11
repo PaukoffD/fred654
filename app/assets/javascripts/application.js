d4f0c4612fa9101a09238a8fc4c241361cfc7161
xuliaP <paukov2967@gmail.com>
2017-04-11 21:43:33 +0300

    add translation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery3
+//= require rails-timeago-all
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require ckeditor/init
